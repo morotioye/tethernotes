23189b4e5a8a327f46039b1a2b44685cb25bc49b
Moroti <morotioye@gmail.com>
2025-02-15 12:37:35 -0800

    themes 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { ThemeProvider } from './components/theme-provider'
+import { ThemeToggle } from './components/theme-toggle'
 
 export default function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-white">
-      <h1 className="text-2xl font-semibold text-black">
-        Hello World
-      </h1>
-    </div>
+    <ThemeProvider defaultTheme="system" storageKey="app-theme">
+      <div className="h-screen w-screen flex flex-col bg-background text-foreground">
+        <div className="flex justify-end p-4">
+          <ThemeToggle />
+        </div>
+        <div className="flex-1 flex items-center justify-center">
+          <h1 className="text-2xl font-semibold">
+            Hello World
+          </h1>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 } 
